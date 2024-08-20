6202a8f8d15fcaa191dbca89d501f5984b44f573
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-20 21:19:26 +0300

    declare JLabel

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -4,6 +4,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class MyFrame extends JFrame implements KeyListener {
+    JLabel label;
 
     MyFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
